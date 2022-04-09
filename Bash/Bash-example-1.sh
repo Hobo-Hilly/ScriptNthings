d976678bc4c0c192875e0b3c3d44d06229c485ce
Hilly@localhost <vincentcid96@gmail.com>
2022-04-09 20:42:34 +0000

    Commenting and cleaning

--- Bash/Bash-example-1.sh
+++ Bash/Bash-example-1.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-var1=$(cat /etc/passwd | awk '{FS = ":"} {print $1 }')
+var1=$(cat /etc/passwd | awk '{FS = ":"} {print $1 }')   	# setting var1 and giving it the attribute to cat /etc/passwd | then using the awk command to manipulate the output. changing the field seperator and then printing the first field after  
 
-for i in $var1
+for i in $var1				# using 'i' (for iterate) to iterate through the contents of the contents in '$var1'
 
-do
+do		# stating a do statement
 
-	echo "The next user name in the system is..." $i
+	echo "The next user name in the system is..." $i	# prints to the screen and calls variable 'i' to the screen
 
-done
+done		# have to end every do statement with 'done'
--- Bash/Bash-example-2.sh
+++ Bash/Bash-example-2.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-for (( a = 1 ; a <= 11; a++))
+for (( a = 1 ; a <= 11; a++))		#  a 'for' loop where we create the var 'a' ; then create the condition we want (as long as a is less then or equal to 11) ;  then a is having 1 added to it each time it passes bby
 
-do 
-	echo "Counting is fun $a"
+do 		# starts a do statement
+	echo "Counting is fun $a"		# prints text to the screen and the calls '$a' to the screen each time 
 
-done
+done			# must end your do statement with 'done'
+
+
+# this is a 'C style' for loop
